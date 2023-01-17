70b45118de2cfc172324bf69bf6990380bf89e28
LEBBARABDELHADI <lebbar1996@gmail.com>
2023-01-17 23:39:41 +0100

    refresh js

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ReactUdemyCouurse
+# ReactUdemyCourse
---
+++ javascript section/core.js
@@ -0,0 +1,18 @@
+console.log("test");
+
+function printName(name){
+    console.log("name "+name);
+}
+
+
+printName("David")
+
+const printlastname = (lastname) =>{
+    console.log("last name "+lastname );
+}
+printlastname("lebbar");
+
+// // const multiply = number => {return number*2;};
+const multiply = number =>  number*2;
+
+console.log(multiply(2));
